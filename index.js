043afb9bc9a4fdb9e4c1a998f70536d0f3acc490
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 10:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
