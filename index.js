577b8a4d608b144971b71d6e15d64e0ad9fb7325
Felix Godfree <felixfgodfree@gmail.com>
2018-08-27 10:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();
   return kittens;
 }
+
+function appendKitten(name) {
+  var appKitten = [...kittens, name];
+  return appKitten;
+}
